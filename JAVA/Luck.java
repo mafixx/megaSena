da3ccf1674a38e4633b30b1e5fe4958466211092
Pedro Barbieri <pedro.romaguera@hotmail.com>
2023-12-19 17:51:43 -0300

    adding files

---
+++ JAVA/Luck.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Luck {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=1; i<61; i++) {
+            list.add(Integer.valueOf(i));
+        }
+        Collections.shuffle(list);
+        ArrayList<Integer> megasena = new ArrayList<Integer>(list.subList(0, 6));
+        Collections.sort(megasena);
+        System.out.println(megasena);
+    }
+}
---
+++ Python/Luck.py
@@ -0,0 +1,5 @@
+import random
+
+megasena = random.sample(range(1, 61), 6)
+megasena.sort()
+print(megasena)
